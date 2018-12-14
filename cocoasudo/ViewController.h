8c87e1afb07e1bfd9cf092ac754caf8a6ab952a2
npyl <n.pylarinos@hotmail.com>
2018-12-14 15:38:40 +0200

    Work for converting from console to GUI project

---
+++ cocoasudo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cocoasudo
+//
+//  Created by Nickolas Pylarinos Stamatelatos on 14/12/2018.
+//  Copyright © 2018 Nickolas Pylarinos Stamatelatos. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ cocoasudo/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  cocoasudo
+//
+//  Created by Nickolas Pylarinos Stamatelatos on 14/12/2018.
+//  Copyright © 2018 Nickolas Pylarinos Stamatelatos. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
